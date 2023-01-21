69840608c44ccad05d79113fd26e821ff72ae550
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-01-20 22:45:20 -0400

    1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-print(f"{a} + {b} = {add(a, b)}")
-print(f"{a} - {b} = {add(a, b)}")
-print(f"{a} * {b} = {add(a, b)}")
-print(f"{a} / {b} = {add(a, b)}")
+print("{} {} {} = {}".format(a, plus, b, add(a, b)))
+print("{} {} {} = {}".format(a, minus, b, sub(a, b)))
+print("{} {} {} = {}".format(a, multi, b, mul(a, b)))
+print("{} {} {} = {}".format(a, divid, b, div(a, b)))
