8bfb5bfcbbd5da2ffcb96123be9474dedc81abc7
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-01-23 11:09:18 -0400

    2-replace_iin_list.py

--- python-data_structures/2-replace_iin_list.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def element_at(my_list, idx, element):
-    if idx < 0:
-        return my_list
-    if idx > len(my_list):
-        return my_list
-    return my_list[element]
-    
