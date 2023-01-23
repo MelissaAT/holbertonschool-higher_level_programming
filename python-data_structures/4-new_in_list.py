0584b16656c8a433a1a533239851b8986ca81c39
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-01-23 13:40:15 -0400

    4-new_in_list.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -4,6 +4,6 @@ def new_in_list(my_list, idx, element):
     if idx < 0:
         return (my_list)
     if idx > len(my_list) - 1:
-        return (my_list)   
+        return (my_list)
     new_in_list[idx] = element
     return (new_in_list)
