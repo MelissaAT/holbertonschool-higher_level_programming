bfa52accbce04586ea6aec38c72df617fc49cc5a
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-01-23 19:12:54 -0400

    6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    ef print_matrix_integer(matrix=[[]]):
-    print("\n".join(" ".join(str(elem) for elem in row) for row in matrix) , end = '\n--\n' )
+    for a in matrix:
+        print(" ".join("{:d}".format(row) for row in a))
