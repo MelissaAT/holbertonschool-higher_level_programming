ba814e53e2d98a407a1c31efad31d8fc1dfeff16
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-01-23 18:45:04 -0400

    5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    characters = my_string.translate({ord(a): None for i in 'cC'})
-    return (characters)
+    characters = my_string.translate({ord(a): None for a in 'cC'})
+    return characters
