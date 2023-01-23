a2bbffc14e58bde43b406c1d54c3e0a62b20220e
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-01-23 13:31:52 -0400

    4-new_in_list.py

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/binpython3
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    if idx > len(my_list) - 1:
+        return my_list
+    for i in range(len(my_list)):
+        if new_in_list[i] == idx:
+            new_in_list = element
+            return new_in_list
