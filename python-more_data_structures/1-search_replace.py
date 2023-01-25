75c68cfcd4d158b4fd16bd1570ff969143f6d811
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-01-25 12:53:11 -0400

    1-search_replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    resutl = my_list.replace("search", "replace")
+    resutl = re.sub("search", "replace", my_list)
     return (result)
     #for replace, search in enumerate(my_list):
         #if search == my_list[replace]:
