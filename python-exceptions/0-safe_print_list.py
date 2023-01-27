b440563c97f7f0d3ebd91feb437803372a70349d
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-01-27 17:48:44 -0400

    0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/pyhton3
 def safe_print_list(my_list=[], x=0):
-    elements_printed = 0
+    count = 0
     try:
         for i in range(x):
             print(my_list[i], end='')
-            elements_printed += 1
+            count =+ 1
         print()
+        return x
     except IndexError:
         print()
-    return elements_printed
+    return count
