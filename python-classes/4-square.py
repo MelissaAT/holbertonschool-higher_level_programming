2b8af7236bcdc36694e2e4a1e4873698daa86c5a
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-01-30 14:24:46 -0400

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -14,12 +14,12 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-   
     @property
     def size(self):
         return self.__size
     @size.setter
     def size(self, value):
+
         if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
