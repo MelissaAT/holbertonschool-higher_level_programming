5da6337149feb3d7b1efa8d83eb0a070ed89a354
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-01-30 11:01:27 -0400

    3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Write a class that defines a square
+"""
+
+
+class Square:
+    """ class square has to be an int
+        area(self) returns the current square area 
+    """
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        _area = self.__size * self.__size
+        return _area
