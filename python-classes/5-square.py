055c28beea32e510a44c60fb3c9fa6f6dd6328d7
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-01-31 11:09:39 -0400

    6-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -14,10 +14,12 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     @property
     def size(self):
         return self.__size
     @size.setter
+
     def size(self, value):
 
         if type(value) != int:
@@ -25,11 +27,13 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def my_print(self):
         if self.__size == 0:
             print()
         for i in range(self.__size):
             print("#" * self.__size)
+
     def area(self):
         _area = self.__size * self.__size
         return _area
