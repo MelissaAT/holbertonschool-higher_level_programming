4859aac6f82f1e3f5a7d350989a33e0ca92fc6a8
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-01-31 11:06:02 -0400

    6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -55,8 +55,6 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
-        for i in self.__position[1]:
-            print()
         for i in range(self.__size):
             print(" " * self.__position[0] + "#" * self.__size)
     def area(self):
