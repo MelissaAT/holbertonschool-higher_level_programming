ecb5789693fc96be554f13254799489b50b9e716
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-01-31 11:15:14 -0400

    6-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -13,6 +13,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         _area = self.__size * self.__size
         return _area
