759803c1662d6a454851acc9a0ca6eb6e853413e
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-02-03 10:36:34 -0400

    2-rectangle.py

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+    Write a class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+    """Class Rectangle defines width and height and raise exeptions
+    """
+    def __init__(self, width=0, height=0):
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        _area = self.__width * self.__height
+        return _area
+    
+    def perimeter(self):
+        return (2 *(self.__width + self.__height))
