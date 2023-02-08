e43e6dea8c6457917a43ff77ea6499cc031f25eb
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-02-08 12:20:25 -0400

    7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -17,4 +17,4 @@ class BaseGeometry:
         if type(value) != int:
             raise TypeError("name must be an integer")
         if value <= 0:
-            raise ValueError("name must be greater than 0")
+            raise ValueError("age must be greater than 0")
