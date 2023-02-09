b9e82b251ecee0b6f3d2a3b036e4b3bc3f4359d3
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-02-09 14:35:11 -0400

    8-rectangle.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -5,7 +5,7 @@ Write a class BaseGeometry
 """
 
 
-Rectangle = __import__('8-rectangle').Rectangle
+Rectangle = __import__('7-rectangle').Rectangle
 class Rectangle(BaseGeometry):
     """Write a class Rectangle that inherits from BaseGeometry"""
     def __init__(self, width, height):
