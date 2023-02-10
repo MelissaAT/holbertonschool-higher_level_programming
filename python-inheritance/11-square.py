a5e6b08b7f92f07ad22487118b6ef01ca9fcef90
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-02-09 20:16:40 -0400

    10-square.py

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -7,7 +7,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Write a class Square that inherits from Rectangle 
+    """Write a class Square that inherits from Rectangle
     (9-rectangle.py). (task based on 10-square.py)."""
 
     def __init__(self, size):
