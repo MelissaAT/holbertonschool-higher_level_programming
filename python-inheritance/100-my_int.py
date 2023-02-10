b9c4f8aeabe9d5325923e3f52431d2da112d8c0f
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-02-09 20:27:21 -0400

    100-my_int.py

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -6,6 +6,6 @@ class MyInt(int):
     """comment"""
     def __eq__(self, other):
         return int(self) != int(other)
-    
+
     def __ne__(self, other):
         return int(self) == int(other)
