8018813af63def00d024dd589bf7125945b72a12
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-02-10 15:18:47 -0400

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -10,5 +10,6 @@ def read_file(filename=""):
     write a function that reads a text file 
     (UFT8) and prints it to stdout
     """
-    with open(filename, encoding="uft-8") as f:
-        print(f.read())
+    with open(filename, encoding="utf-8") as f:
+        a = f.read()
+    print(a)
