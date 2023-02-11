e8086b1aa285eea8c6dc864dfb616ad8496fe5c1
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-02-11 19:19:33 -0400

    9-student.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -6,9 +6,9 @@ class Student:
     """comment class"""
 
 def __init__(self, first_name, last_name, age):
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
 def to_json(self):
-    return self.__dict__
+        return self.__dict__
