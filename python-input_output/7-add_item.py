47f52769cc8042561b67dc1100f0c3d971acc3db
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-02-11 19:00:33 -0400

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
 """comment Module"""
 import sys
-import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-def save_to_json_file(my_obj, filename):
-    with open(filename, 'w') as f:
-        json.dump(my_obj, f)
-
-def load_from_json_file(filename):
-    with open(filename, 'r') as f:
-        return json.load(f)
-
+args = sys.argv
 filename = "add_item.json"
+my_list = []
+
 try:
-    items = load_from_json_file(filename)
+    my_list = load_from_json_file(filename)
 except:
-    items = []
-
-for arg in sys.argv[1:]:
-    items.append(arg)
-
-save_to_json_file(items, filename)
+    pass
+for i in range(1, len(args)):
+    my_list.append(args[i])
+save_to_json_file(my_list, filename)
