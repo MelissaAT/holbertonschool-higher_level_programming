80ca0900abff54334ce965cdc0d307820f66c20a
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-02-11 18:58:27 -0400

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 """comment Module"""
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+import json
+
+def save_to_json_file(my_obj, filename):
+    with open(filename, 'w') as f:
+        json.dump(my_obj, f)
+
+def load_from_json_file(filename):
+    with open(filename, 'r') as f:
+        return json.load(f)
 
 filename = "add_item.json"
 try:
