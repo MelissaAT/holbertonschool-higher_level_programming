a7d1d8c73da47a9fc14f5e18d59a2bfdebb1c446
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-02-11 17:05:23 -0400

    7-add_item.py

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""write a script that adds all arguments to a python lis"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """comment function"""
+    with open(filename, 'w') as f:
+        json.dump(my_obj,f)
+
+def load_from_json_file(filename):
+    """comment function"""
+    with open(filename, 'r') as f:
+        return json.load(f)
+
+filename = "add_item.json"
+
+try:
+    items = load_from_json_file(filename)
+except:
+    items = []
+
+for arg in sys.argv[1:]:
+    items.append(arg)
+
+save_to_json_file(items,filename)
