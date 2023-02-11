057e9fa3eaa05730a58620b954d4f2cad266abb5
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-02-11 19:34:12 -0400

    10-student.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""comment module"""
+
+#!/usr/bin/python3
+"""comment Mudoule"""
+
+
+class Student():
+    """comment class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        obj_dict = {}
+        if attrs is None:
+            attrs = dir(self)
+        for i in attrs:
+            if i in dir(self):
+                obj_dict[i] = getattr(self, attr)
+            return obj_dict
