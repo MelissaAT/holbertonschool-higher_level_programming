6a54782c5721dcf765457027c3e5bd227c5de903
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-02-11 19:58:09 -0400

    11-student.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -21,4 +21,4 @@ class Student():
     
     def reload_from_json(self, json):
         for key, value in json.items():
-            setattr(self, key, value)
+            self.__dict__[key] = value
