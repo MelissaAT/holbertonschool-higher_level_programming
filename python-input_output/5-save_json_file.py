4dcb3c355057062b030765598f5cc3c9feb4f9c5
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-02-11 16:14:46 -0400

    5-save_json_file.py

--- python-input_output/5-save_json_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""comentario modulo"""
-import json
-
-
-def save_to_json_file(my_obj, filename):
-    """comment definicion"""
-    with open('filename', 'w') as f:
-        json.dum(my_obj, f)
