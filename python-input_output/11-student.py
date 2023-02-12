80bfd19650723e2c6be2083d5fe066bf4721ccb7
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-02-11 20:00:27 -0400

    11-student.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -2,24 +2,22 @@
 """Module 11-student"""
 
 
-class Student():
-    """Class for student"""
-
+class Student:
     def __init__(self, first_name, last_name, age):
-        """Instantiation of student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retreives dictionary representation of Student instance"""
-        if isinstance(attrs, list) and all(
-            isinstance(elements, str) for elements in attrs
-        ):
-            return {k: v for k, v in self.__dict__.items() if k in attrs}
-        return self.__dict__
+        obj_dict = {}
+        if attrs is None:
+            attrs = dir(self)
+        for i in attrs:
+            if i in dir(self):
+                obj_dict[i] = getattr(self, i)
+        return obj_dict
 
     def reload_from_json(self, json):
-        """Replace all attributes of the Student instance"""
-        for k, v in json.items():
-            self.__dict__[k] = v
+        for key, value in json.items():
+            setattr(self, key, value)
+
