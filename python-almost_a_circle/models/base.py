34c5dbf178d34a153cae7eb30da60ce567665640
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-02-13 11:15:05 -0400

    first files

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -3,11 +3,13 @@
 
 
 class Base:
-    """comment class"""
+    """This class will be the “base” of all other classes in this project. The goal of it is to manage id attribute in all your 
+    future classes and to avoid duplicating the same code"""
     __nb_objects = 0
     def __init__(self, id=None):
         if id != None:
             self.__id = id
-    
         else:
-            Base.__nb_objects = self.__id
+            
+            Base.__nb_objects += 1
+            self.__id = Base.__nb_objects
