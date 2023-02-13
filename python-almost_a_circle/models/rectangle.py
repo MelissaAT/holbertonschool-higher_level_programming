87fafae2c95d168a1cce18b7d3b6becd69efbb7f
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-02-13 14:19:21 -0400

    rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -6,9 +6,27 @@ from models.base import Base
 class Rectangle(Base):
     """Class rectangle inherits from Base"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        if width is not int:
+            TypeError("width must be an integer")
+        elif width < 0:
+            ValueError("width must be > 0")
         self.__width = width
+        if height is not int:
+            TypeError("height must be an integer")
+        elif height < 0:
+            ValueError("height must be > 0")
         self.__height = height
+
+        if x is not int:
+            TypeError("x must be an integer")
+        elif x < 0:
+            ValueError("x must be > 0")
         self.__x = x
+
+        if y is not int:
+            TypeError("y must be an integer")
+        elif y < 0:
+            ValueError("y must be > 0")
         self.__y = y
         super().__init__(id)
 
@@ -18,7 +36,11 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        self.__width = value
+        if value is not int:
+            TypeError("width must be an integer")
+        elif value < 0:
+            ValueError("width must be > 0")
+            self.__width = value
 
     @property
     def height(self):
@@ -26,6 +48,10 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        if value is not int:
+            TypeError("height must be an integer")
+        elif value < 0:
+            ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -34,6 +60,10 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        if value is not int:
+            TypeError("x must be an integer")
+        elif value < 0:
+            ValueError("x must be > 0")
         self.__x = value
 
     @property
@@ -42,4 +72,8 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        if value is not int:
+            TypeError("y must be an integer")
+        elif value < 0:
+            ValueError("y must be > 0")
         self.__y = value
