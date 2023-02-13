1901f180924c387e424f95545cf7acdf4b8e90f3
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-02-13 14:51:16 -0400

    rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -68,9 +68,9 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         if type(value) is not int:
-            TypeError("x must be an integer")
+            raise TypeError("x must be an integer")
         if value < 0:
-            ValueError("x must be > 0")
+            raise ValueError("x must be > 0")
         self.__x = value
 
     @property
