4547c09a863b1195f53f1693c348f60090598f6c
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-02-13 13:22:31 -0400

    rectangles.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -5,12 +5,13 @@ from models.base import Base
 
 class Reactangle(Base):
     """Class rectangle inherits from Base"""
-def __init__(self, width, height, x=0, y=0, id=None):
-    self.__width = width
-    self.__height = height
-    self.__x = x
-    self.__y = y
-    Base.__init__(id)
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super.__init__(id)
+
     @property
     def width(self):
         return self.__width
