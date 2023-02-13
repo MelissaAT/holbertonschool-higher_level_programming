65e35a27e29fd906be5d39c25cb383020f128f8a
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-02-13 11:20:39 -0400

    first files

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,8 +4,10 @@
 
 class Base:
     """This class will be the “base” of all other classes in this project. The goal of it is to manage id attribute in all your 
-    future classes and to avoid duplicating the same code"""
+    future classes and to avoid duplicating
+    the same code"""
     __nb_objects = 0
+
     def __init__(self, id=None):
         if id is None:
             Base.__nb_objects += 1
