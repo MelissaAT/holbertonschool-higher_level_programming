46810d9e351899add4f8be60abf3352f3dfe0b9d
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-02-13 13:28:00 -0400

    base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -8,11 +8,9 @@ class Base:
     to manage id attribute in all your
     future classes and to avoid duplicating
     the same code"""
-    __nb_objects = 0
-
     def __init__(self, id=None):
-        if id is None:
+        if id is not None:
+            self.id = id
+        else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-        else:
-            self.id = id
