069995da1b22eb58763a8218f3ea7f15ab283634
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-02-14 12:55:54 -0400

    rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -93,5 +93,4 @@ class Rectangle(Base):
 
     def __str__(self):
         """comment function"""
-        return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} -"
-        f"{self.__width}/{self.__height}")
+        return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}")
