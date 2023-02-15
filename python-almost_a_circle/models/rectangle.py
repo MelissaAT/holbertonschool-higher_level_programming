17b32cb15992157a599c050a00e1d475fccf0c34
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-02-14 20:27:55 -0400

    rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -97,4 +97,17 @@ class Rectangle(Base):
 
     def __str__(self):
         """comment function"""
-        return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}")
+        return (f'[Rectangle] ({self.id}) {self.x}/{self.y} - '
+                f'{self.width}/{self.height}')
+    
+    def update(self, *args):
+        if args:
+            self.__id = args[0]
+        if len(args) > 1:
+            self.__width = args[1]
+        if len(args) > 2:
+            self.__height = args[2]
+        if len(args) > 3:
+            self.__x = args[3]
+        if len(args) > 4:
+            self.__y = args[4]
