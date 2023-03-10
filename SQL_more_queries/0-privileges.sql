e030e278cf24e5ef67afb15023511902581ae83d
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-03-10 10:25:31 -0400

    0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
--- Create new user
-CREATE USER 'user_od_1' @ 'localhost' 
-IDENTIFIED BY 'password';
-CREATE USER 'user_od_2' @ 'localhost' 
-IDENTIFIED BY 'password';
+-- script that lists all privileges of the MySQL users 
+SHOW GRANTS FOR 'user_0d_1' @ 'localhost';
+SHOW GRANTS FOR 'user_0d_2' @ 'localhost';
+
+
