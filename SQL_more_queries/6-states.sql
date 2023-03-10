8690739f27dbea6ef5636ebbd3d3b22435061ed3
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-03-10 15:33:58 -0400

    6-states.sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- script that creates the database hbtn_0d_usa and the table states
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa.states;
-CREATE TABLE IF NOT EXISTS states (
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
     id INT SERIAL DEFAULT VALUE primary key,
     name VARCHAR(256)
 ); 
