2090aa23106dbade1ef7ebdd84f6bc84ee6a8166
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-03-10 10:37:35 -0400

    1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- Create user
-CREATE USER IF NOT EXISTS 'user_od_1'@'localhost' 
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' 
 IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_01_1'@'localhost' WITH GRANT
