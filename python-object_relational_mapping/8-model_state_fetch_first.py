16b88cfa462984c8615f5a9e7fd2ae990e2e3f2e
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-03-16 19:14:05 -0400

    8-model_state_fetch_first.py

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""Start link class to table in database"""
+"""
+Script that prints the first State
+object from the database
+"""
 import sys
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 
-if __name__ == "__main__":
-    db_api = 'mysql+mysqldb://{}:{}@localhost/{}'
-    usr = sys.argv[1]
-    pswd = sys.argv[2]
-    db = sys.argv[3]
-
-    engine = create_engine(db_api.format(usr, pswd, db), pool_pre_ping=True)
+if __name__ == '__main__':
+    engine = (create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                            .format(sys.argv[1], sys.argv[2],
+                                    sys.argv[3]), pool_pre_ping=True))
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).first()
-    idx = "{}:"
-
-    if states is not None:
-        print(idx.format(states.id), states.name)
+    first = session.query(State).first()
+    if first:
+        print("{}: {}".format(first.id, first.name))
     else:
         print("Nothing")
+    session.close()
