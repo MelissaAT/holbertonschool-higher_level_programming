cdce558ee528141a51c593949d58eec587172a68
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-03-16 09:29:02 -0400

    2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
                             user=username, passwd=password, db=db_name))
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY'{}'")
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY'{}'".format(state_name))
 
     for row in cursor.fetchall():
         print(row)
