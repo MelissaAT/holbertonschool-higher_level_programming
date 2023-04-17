d9154a71b76261440eb4aa328bb1fe9cec60fee1
Melissa Arroyo <melissaarroyotorres9806@gmail.com>
2023-04-17 14:14:55 -0400

    7-multi_c.js

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,17 +1,14 @@
 #!/usr/bin/node
 
-const args = process.argv;
+const args = parseInt(process.argv[2]);
 
 let count = 0;
-if (args[1]) {
-    console.log('Missing number of occurrences');
-  }
-else {
-while (count < parseInt(args[2])) {
-  count++;
-  console.log('C is fun');
-}
-if (args[1]) {
+
+if (isNaN(args)) {
   console.log('Missing number of occurrences');
-}
+} else {
+  while (count < (args)) {
+    count++;
+    console.log('C is fun');
+  }
 }
