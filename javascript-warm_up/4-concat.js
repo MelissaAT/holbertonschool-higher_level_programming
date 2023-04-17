79bfc463f670cbb86059e0e5d2609c3e7f802449
Melissa Arroyo <melissaarroyotorres9806@gmail.com>
2023-04-17 13:08:02 -0400

    4-concat.js

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -6,9 +6,9 @@ const str = args[2];
 const str1 = args[3];
 
 if (args[2]) {
-  console.log(str, ' is ', str1);
+  console.log(str, 'is', str1);
 } else if (args[3]) {
-  console.log(str, ' is ', str1);
+  console.log(str, 'is', str1);
 } else {
   console.log('undefined is undefined');
 }
