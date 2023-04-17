3c59d1f4639f7407b03ac9f917af31d18f5ef5c9
Melissa Arroyo <melissaarroyotorres9806@gmail.com>
2023-04-17 13:48:03 -0400

    7-multi_c.js

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -3,11 +3,15 @@
 const args = process.argv;
 
 let count = 0;
-
+if (args[1]) {
+    console.log('Missing number of occurrences');
+  }
+else {
 while (count < parseInt(args[2])) {
   count++;
   console.log('C is fun');
 }
 if (args[1]) {
   console.log('Missing number of occurrences');
+}
 }
