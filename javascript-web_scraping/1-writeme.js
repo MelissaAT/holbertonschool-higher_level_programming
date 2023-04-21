6159197bebfb3efdf3b646628641b1abbd540e6c
Melissa Arroyo <melissaarroyotorres9806@gmail.com>
2023-04-21 19:48:25 -0400

    1-writeme.js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -4,10 +4,13 @@ const fs = require('fs');
 
 function writeFiles (filePath, content) {
   fs.writeFile(filePath, content, 'utf-8', (err) => {
-    if (err) throw err;
-    console.log(err);
+    if (err) {
+      console.error(err);
+    }
   });
 }
+
 const filePath = process.argv[2];
 const content = process.argv[3];
+
 writeFiles(filePath, content);
