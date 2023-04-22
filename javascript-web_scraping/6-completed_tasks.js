18f7970caadd357aa706ad712ae03861e50e2936
Melissa Arroyo <melissaarroyotorres9806@gmail.com>
2023-04-22 17:59:28 -0400

    6-completed_tasks.js

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const todos = JSON.parse(body);
+    const completedTasks = {};
+
+    for (let i = 0; i < todos.length; i++) {
+      const todo = todos[i];
+      if (todo.completed) {
+        const userId = todo.userId;
+        if (completedTasks[userId] !== undefined) {
+          completedTasks[userId]++;
+        } else {
+          completedTasks[userId] = 1;
+        }
+      }
+    }
+
+    const userIds = Object.keys(completedTasks);
+    for (let i = 0; i < userIds.length; i++) {
+      const userId = userIds[i];
+      console.log(`${userId}: ${completedTasks[userId]}`);
+    }
+  }
+});
